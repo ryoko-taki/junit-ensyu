9e4290661e89ba5091b0046c3982fe00a91bb855
ryoko-taki <ryoko.taki@rakus-partners.co.jp>
2022-08-15 13:43:39 +0900

    Calculatorテストの作成

--- src/test/java/junit/tutorial/ex01/e02/CalculatorTest.java
+++ src/test/java/junit/tutorial/ex01/e02/CalculatorTest.java
@@ -2,8 +2,12 @@ package junit.tutorial.ex01.e02;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,8 +31,9 @@ class CalculatorTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCalculator() {
+		List<Calculator> it = new ArrayList<>();
+		assertThrows(Exception.class, () -> it.get(0));
 	}
 
 }
