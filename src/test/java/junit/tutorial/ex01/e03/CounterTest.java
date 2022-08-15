52c7fbde9290d367e93e04c5194b25613df9dcd7
ryoko-taki <ryoko.taki@rakus-partners.co.jp>
2022-08-15 14:26:38 +0900

    counterテストの作成

--- src/test/java/junit/tutorial/ex01/e03/CounterTest.java
+++ src/test/java/junit/tutorial/ex01/e03/CounterTest.java
@@ -1,18 +1,14 @@
-package junit.tutorial.ex01.e02;
+package junit.tutorial.ex01.e03;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class CalculatorTest {
+class CounterTest {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -30,11 +26,28 @@ class CalculatorTest {
 	void tearDown() throws Exception {
 	}
 
+	Counter counter = new Counter();	
 	@Test
-	void testCalculator() {
-		Calculator calculator = new Calculator();
-		assertThrows(IllegalArgumentException.class, 
-				() -> {calculator.divide(5, 0);} );
+	void testIncrement1() {
+		int result = counter.increment();
+		assertEquals(1, result);
+	}
+	
+	@Test
+	void testIncrement2() {
+		counter.increment();
+		int result = counter.increment();
+		assertEquals(2, result);
+	}
+	
+	@Test
+	void testIncrement3() {
+		for(int i = 1; i <= 50; i++) {
+			counter.increment();
+		}
+		
+		int result = counter.increment();
+		assertEquals(51, result);
 	}
 
 }
