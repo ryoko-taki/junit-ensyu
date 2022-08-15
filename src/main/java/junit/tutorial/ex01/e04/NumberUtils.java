40dbabcf3d1779734fe93b6e889c4a00eb52e05a
ryoko-taki <ryoko.taki@rakus-partners.co.jp>
2022-08-15 14:38:37 +0900

    NumberUtilテストの作成

--- src/main/java/junit/tutorial/ex01/e04/NumberUtils.java
+++ src/main/java/junit/tutorial/ex01/e04/NumberUtils.java
@@ -5,5 +5,10 @@ public class NumberUtils {
 	public static boolean isEven(int num) {
 		return (num % 2 == 0);
 	}
+	
+//	public static void main(String[] args) {
+//	    System.out.println(isEven(10));
+//		System.out.println(isEven(7));
+//	}
 
 }
---
+++ src/test/java/junit/tutorial/ex01/e04/NumberUtilsTest.java
@@ -0,0 +1,41 @@
+package junit.tutorial.ex01.e04;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NumberUtilsTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIsEven1() {
+		boolean test1 = NumberUtils.isEven(10);
+		assertEquals(true, test1, "Error");
+	}
+	
+	@Test
+	void testIsEven2() {
+		boolean test2 = NumberUtils.isEven(7);
+		assertEquals(false, test2, "Error");
+	}
+
+}
